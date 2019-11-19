62575f361cb80bea905efcee83787de35b5f583a
benoitgallet <benoit.gallet@live.fr>
2019-11-19 11:54:26 -0700

    debug

--- main.cu
+++ main.cu
@@ -69,6 +69,8 @@ int main(int argc, char * argv[])
     unsigned int DBSIZE = NDdataPoints.size();
     setQueueIndexCPU(DBSIZE);
 
+    sortInNDBins(&NDdataPoints);
+
     printf("Converting the dataset for Super-EGO\n");
     Point * A = new Point[DBSIZE + 1];
     for(int i = 0; i < DBSIZE; ++i)
@@ -81,8 +83,6 @@ int main(int argc, char * argv[])
         }
     }
     Point * B = A;
-
-    sortInNDBins(&NDdataPoints);
 
     DTYPE * minArr = new DTYPE [NUMINDEXEDDIM];
     DTYPE * maxArr = new DTYPE [NUMINDEXEDDIM];
