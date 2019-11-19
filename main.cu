71d91c25a290798eb6ba7dbdef4af6003a07d22c
benoitgallet <benoit.gallet@live.fr>
2019-11-19 12:01:30 -0700

    debug

--- main.cu
+++ main.cu
@@ -213,6 +213,12 @@ int main(int argc, char * argv[])
                     egoMapping[p->id] = i;
                 }
 
+                for(int i = 0; i < 10; i++)
+                {
+                    printf("[TEST | %d] ~ Dataset = %f, %f | Point = %f, %f | Index = %d | Mapping = %d\n",
+                        dataset[i * GPUNUMDIM], [i * GPUNUMDIM + 1], &A[i]->x[0], &A[i]->x[1], originPointIndex[i], egoMapping[i]);
+                }
+
                 totalNeighborsCPU = Util::multiThreadJoinWorkQueue(A, A_sz, B, B_sz, CPU_THREADS, egoMapping);
 
                 double tEndEgo = omp_get_wtime();
