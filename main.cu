568658a51053cf9a344fe3a10629c78fdbd61c04
benoitgallet <benoit.gallet@live.fr>
2019-11-21 16:16:01 -0700

    test

--- main.cu
+++ main.cu
@@ -218,8 +218,8 @@ int main(int argc, char * argv[])
                 {
                     unsigned int tid = omp_get_thread_num();
                     unsigned int size = A_sz / CPU_THREADS;
-                    unsigned int min = min(tid * size, A_sz);
-                    std::stable_sort(A, A + min, egoSortFunction);
+                    unsigned int end = min(tid * size, A_sz);
+                    std::stable_sort(A, A + end, egoSortFunction);
                 }
                 std::stable_sort(A, A_sz, egoSortFunction);
 
