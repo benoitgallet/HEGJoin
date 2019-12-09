463b3e5540cd7e5f1d2ddd70edd9a9be475ae3c1
benoitgallet <benoit.gallet@live.fr>
2019-12-09 03:17:29 -0700

    changing epsilon values for the SDSS bench script

--- plots/bench_sdss.sh
+++ plots/bench_sdss.sh
@@ -4,85 +4,85 @@ SDSS="~/datasets/2d/sdss_2d_15m_normalized.txt"
 
 echo '~ SDSS2D15M'
 echo '# GPU'
-echo '## Epsilon 0.000055556'
-./main $SDSS 0.000055556 2 0 | grep "RESULT"
-./main $SDSS 0.000055556 2 0 | grep "RESULT"
-./main $SDSS 0.000055556 2 0 | grep "RESULT"
+echo '## Epsilon 0.0004'
+./main $SDSS 0.0004 2 0 | grep "RESULT"
+./main $SDSS 0.0004 2 0 | grep "RESULT"
+./main $SDSS 0.0004 2 0 | grep "RESULT"
 echo
-echo '## Epsilon 0.000111111'
-./main $SDSS 0.000111111 2 0 | grep "RESULT"
-./main $SDSS 0.000111111 2 0 | grep "RESULT"
-./main $SDSS 0.000111111 2 0 | grep "RESULT"
+echo '## Epsilon 0.0008'
+./main $SDSS 0.0008 2 0 | grep "RESULT"
+./main $SDSS 0.0008 2 0 | grep "RESULT"
+./main $SDSS 0.0008 2 0 | grep "RESULT"
 echo
-echo '## Epsilon 0.000166667'
-./main $SDSS 0.000166667 2 0 | grep "RESULT"
-./main $SDSS 0.000166667 2 0 | grep "RESULT"
-./main $SDSS 0.000166667 2 0 | grep "RESULT"
+echo '## Epsilon 0.0012'
+./main $SDSS 0.0012 2 0 | grep "RESULT"
+./main $SDSS 0.0012 2 0 | grep "RESULT"
+./main $SDSS 0.0012 2 0 | grep "RESULT"
 echo
-echo '## Epsilon 0.000222222'
-./main $SDSS 0.000222222 2 0 | grep "RESULT"
-./main $SDSS 0.000222222 2 0 | grep "RESULT"
-./main $SDSS 0.000222222 2 0 | grep "RESULT"
+echo '## Epsilon 0.0016'
+./main $SDSS 0.0016 2 0 | grep "RESULT"
+./main $SDSS 0.0016 2 0 | grep "RESULT"
+./main $SDSS 0.0016 2 0 | grep "RESULT"
 echo
-echo '## Epsilon 0.000277778'
-./main $SDSS 0.000277778 2 0 | grep "RESULT"
-./main $SDSS 0.000277778 2 0 | grep "RESULT"
-./main $SDSS 0.000277778 2 0 | grep "RESULT"
+echo '## Epsilon 0.0020'
+./main $SDSS 0.0020 2 0 | grep "RESULT"
+./main $SDSS 0.0020 2 0 | grep "RESULT"
+./main $SDSS 0.0020 2 0 | grep "RESULT"
 
 echo
 echo
 
 echo '# Hybrid'
-echo '## Epsilon 0.000055556'
-./main $SDSS 0.000055556 2 1 | grep "RESULT"
-./main $SDSS 0.000055556 2 1 | grep "RESULT"
-./main $SDSS 0.000055556 2 1 | grep "RESULT"
+echo '## Epsilon 0.0004'
+./main $SDSS 0.0004 2 1 | grep "RESULT"
+./main $SDSS 0.0004 2 1 | grep "RESULT"
+./main $SDSS 0.0004 2 1 | grep "RESULT"
 echo
-echo '## Epsilon 0.000111111'
-./main $SDSS 0.000111111 2 1 | grep "RESULT"
-./main $SDSS 0.000111111 2 1 | grep "RESULT"
-./main $SDSS 0.000111111 2 1 | grep "RESULT"
+echo '## Epsilon 0.0008'
+./main $SDSS 0.0008 2 1 | grep "RESULT"
+./main $SDSS 0.0008 2 1 | grep "RESULT"
+./main $SDSS 0.0008 2 1 | grep "RESULT"
 echo
-echo '## Epsilon 0.000166667'
-./main $SDSS 0.000166667 2 1 | grep "RESULT"
-./main $SDSS 0.000166667 2 1 | grep "RESULT"
-./main $SDSS 0.000166667 2 1 | grep "RESULT"
+echo '## Epsilon 0.0012'
+./main $SDSS 0.0012 2 1 | grep "RESULT"
+./main $SDSS 0.0012 2 1 | grep "RESULT"
+./main $SDSS 0.0012 2 1 | grep "RESULT"
 echo
-echo '## Epsilon 0.000222222'
-./main $SDSS 0.000222222 2 1 | grep "RESULT"
-./main $SDSS 0.000222222 2 1 | grep "RESULT"
-./main $SDSS 0.000222222 2 1 | grep "RESULT"
+echo '## Epsilon 0.0016'
+./main $SDSS 0.0016 2 1 | grep "RESULT"
+./main $SDSS 0.0016 2 1 | grep "RESULT"
+./main $SDSS 0.0016 2 1 | grep "RESULT"
 echo
-echo '## Epsilon 0.000277778'
-./main $SDSS 0.000277778 2 1 | grep "RESULT"
-./main $SDSS 0.000277778 2 1 | grep "RESULT"
-./main $SDSS 0.000277778 2 1 | grep "RESULT"
+echo '## Epsilon 0.0020'
+./main $SDSS 0.0020 2 1 | grep "RESULT"
+./main $SDSS 0.0020 2 1 | grep "RESULT"
+./main $SDSS 0.0020 2 1 | grep "RESULT"
 
 echo
-echo 
+echo
 
 echo '# Super-EGO'
-echo '## Epsilon 0.000055556'
-./main $SDSS 0.000055556 2 2 | grep "RESULT"
-./main $SDSS 0.000055556 2 2 | grep "RESULT"
-./main $SDSS 0.000055556 2 2 | grep "RESULT"
+echo '## Epsilon 0.0004'
+./main $SDSS 0.0004 2 2 | grep "RESULT"
+./main $SDSS 0.0004 2 2 | grep "RESULT"
+./main $SDSS 0.0004 2 2 | grep "RESULT"
 echo
-echo '## Epsilon 0.000111111'
-./main $SDSS 0.000111111 2 2 | grep "RESULT"
-./main $SDSS 0.000111111 2 2 | grep "RESULT"
-./main $SDSS 0.000111111 2 2 | grep "RESULT"
+echo '## Epsilon 0.0008'
+./main $SDSS 0.0008 2 2 | grep "RESULT"
+./main $SDSS 0.0008 2 2 | grep "RESULT"
+./main $SDSS 0.0008 2 2 | grep "RESULT"
 echo
-echo '## Epsilon 0.000166667'
-./main $SDSS 0.000166667 2 2 | grep "RESULT"
-./main $SDSS 0.000166667 2 2 | grep "RESULT"
-./main $SDSS 0.000166667 2 2 | grep "RESULT"
+echo '## Epsilon 0.0012'
+./main $SDSS 0.0012 2 2 | grep "RESULT"
+./main $SDSS 0.0012 2 2 | grep "RESULT"
+./main $SDSS 0.0012 2 2 | grep "RESULT"
 echo
-echo '## Epsilon 0.000222222'
-./main $SDSS 0.000222222 2 2 | grep "RESULT"
-./main $SDSS 0.000222222 2 2 | grep "RESULT"
-./main $SDSS 0.000222222 2 2 | grep "RESULT"
+echo '## Epsilon 0.0016'
+./main $SDSS 0.0016 2 2 | grep "RESULT"
+./main $SDSS 0.0016 2 2 | grep "RESULT"
+./main $SDSS 0.0016 2 2 | grep "RESULT"
 echo
-echo '## Epsilon 0.000277778'
-./main $SDSS 0.000277778 2 2 | grep "RESULT"
-./main $SDSS 0.000277778 2 2 | grep "RESULT"
-./main $SDSS 0.000277778 2 2 | grep "RESULT"
+echo '## Epsilon 0.0020'
+./main $SDSS 0.0020 2 2 | grep "RESULT"
+./main $SDSS 0.0020 2 2 | grep "RESULT"
+./main $SDSS 0.0020 2 2 | grep "RESULT"
