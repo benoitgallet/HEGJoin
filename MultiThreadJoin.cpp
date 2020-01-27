a9f74f77f0801e7813703c1438cf3af4e0fe6369
benoitgallet <benoit.gallet@live.fr>
2020-01-27 16:27:04 -0700

    debug

--- MultiThreadJoin.cpp
+++ MultiThreadJoin.cpp
@@ -85,7 +85,7 @@ uint64_t Util::multiThreadJoinWorkQueue(
 				for(unsigned int i = cpuBatch.first; i < cpuBatch.second; ++i)
 				{
 					unsigned int tmpIndex = originPointIndex[i];
-					neighborTable[tmpIndex].dataPtr = resultVector;
+					neighborTable[tmpIndex].dataPtr = &resultVector;
 				}
 				results[tid] += resultVector->size();
 				resultVector->shrink_to_fit();
@@ -145,7 +145,7 @@ uint64_t Util::multiThreadJoinWorkQueue(
 				for(unsigned int i = cpuBatch.first; i < cpuBatch.second; ++i)
 				{
 					unsigned int tmpIndex = originPointIndex[i];
-					neighborTable[tmpIndex].dataPtr = resultVector;
+					neighborTable[tmpIndex].dataPtr = &resultVector;
 				}
 				results[tid] += resultVector->size();
 				resultVector->shrink_to_fit();
