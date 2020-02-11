2d61d412dbede8c6f61134b10b6326dc668754fa
benoitgallet <benoit.gallet@live.fr>
2020-02-11 15:56:22 -0700

    individual result files and scripts

---
+++ plots_fixed/make_plots.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# parallel python3 ::: delta_2m.py delta_10m.py ego_sort.py
+parallel python3 ::: expo2d2m.py expo3d2m.py expo4d2m.py expo6d2m.py expo8d2m.py
+parallel python3 ::: expo2d10m.py expo3d10m.py expo4d10m.py expo6d10m.py expo8d10m.py
+parallel python3 ::: sw2da.py sw2db.py sw3da.py sw3db.py
+parallel python3 ::: sw2da_ego.py sw2db_ego.py sw3da_ego.py sw3db_ego.py
+
+mv *.pdf figures/
---
+++ plots_fixed/sw3db_ego.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import math
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+
+# IMPORT MY LATEX SO I CAN USE \TEXTSC
+import matplotlib as mpl
+mpl.rc('text', **{'usetex':True})
+
+#
+
+egoLabel=r'\textsc{Modified Super-EGO}'
+ego_origLabel=r'\textsc{Original Super-EGO}'
+
+
+
+def getColumn(filename, column):
+    results = csv.reader(open(filename), delimiter=",")
+    next(results, None)  # skip the headers
+    return [result[column] for result in results]
+
+
+
+#GPU_K_SuSy = getColumn("Time_vs_K_SuSy_GPU.txt",0)
+#GPU_Time_SuSy= getColumn("Time_vs_K_SuSy_GPU.txt",1)
+epsilon = getColumn("sw3db.txt", 0)
+ego = getColumn("sw3db.txt", 3)
+egoOrig = getColumn("sw3db.txt", 4)
+
+
+
+#GPU_K_SuSy=np.asfarray(GPU_K_SuSy,dtype=float)
+#GPU_Time_SuSy=np.asfarray(GPU_Time_SuSy,dtype=float)
+epsilon = np.asfarray(epsilon, dtype=float)
+ego = np.asfarray(ego, dtype=float)
+egoOrig = np.asfarray(egoOrig, dtype=float)
+
+
+#Susy
+
+fig = plt.figure(figsize=(5,3))
+ax1 = fig.add_subplot(111)
+
+# We change the fontsize of minor ticks label
+ax1.tick_params(which='major', labelsize=20)
+
+#ax1.plot(GPU_K_SuSy, GPU_Time_SuSy, ls='-',   c='red', marker='o', markersize=10, linewidth=4, label=gpu)
+ax1.plot(epsilon, egoOrig, ls='-', c='dodgerblue', marker='v', markersize=10, linewidth=4, label=ego_origLabel)
+ax1.plot(epsilon, ego, ls='-', c='red', marker='^', markersize=10, linewidth=4, label=egoLabel)
+
+#turn off scientific notation
+plt.ticklabel_format(useOffset=False)
+
+ax1.set_ylim(0,60)
+
+ax1.set_xticks(epsilon)
+
+ax1.set_xlabel(r'$\epsilon$', fontsize=18)
+ax1.set_ylabel('Time (s)', fontsize=18)
+
+ax1.legend(fontsize=14, loc='upper left', fancybox=False, framealpha=1, handlelength=2, ncol=1)
+
+plt.tight_layout()
+print("Saving figure: sw3db_ego.pdf")
+fig.savefig("sw3db_ego.pdf", bbox_inches='tight')
