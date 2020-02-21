5507ea18535e226a08c55a8a236f8027d556628e
benoitgallet <benoit.gallet@live.fr>
2020-02-21 13:09:38 -0700

    debug

--- import_dataset.cpp
+++ import_dataset.cpp
@@ -85,7 +85,7 @@ void importNDDatasetBinary(std::vector< std::vector<DTYPE> > * dataPoints, char 
 
     for(int i = 0; i < 10; i++)
     {
-        std::vector<DTYPE> tmp = dataPoints[i];
+        std::vector<DTYPE> tmp = (*dataPoints)[i];
         for(int j = 0; j < GPUNUMDIM; j++)
         {
             std::cout << tmp[j] << ", ";
