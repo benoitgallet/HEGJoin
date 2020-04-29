a430aee9416028fc0c8da888f7550a3d3914e28a
benoitgallet <benoit.gallet@live.fr>
2020-04-29 02:57:42 -0700

    debug

--- GPU.cu
+++ GPU.cu
@@ -922,7 +922,7 @@ void distanceTableNDGridBatches(
 
 	double tstartbatchest = omp_get_wtime();
     #if SORT_BY_WORKLOAD
-        estimatedNeighbors = GPUBatchEst_v2searchMode, DBSIZE, staticPartition, dev_database, dev_originPointIndex, dev_epsilon, dev_grid, dev_indexLookupArr,
+        estimatedNeighbors = GPUBatchEst_v2(searchMode, DBSIZE, staticPartition, dev_database, dev_originPointIndex, dev_epsilon, dev_grid, dev_indexLookupArr,
                 dev_gridCellLookupArr, dev_minArr, dev_nCells, dev_nNonEmptyCells, &numBatches, &GPUBufferSize, &batchesVector);
     #else
         estimatedNeighbors = GPUBatchEst_v2(searchMode, DBSIZE, staticPartition, dev_database, nullptr, dev_epsilon, dev_grid, dev_indexLookupArr,
