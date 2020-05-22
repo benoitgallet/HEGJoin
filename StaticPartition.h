c08d13442db3be8e53a29db43ee812962aa20f7f
benoitgallet <benoit.gallet@live.fr>
2020-05-22 01:45:13 -0700

    debug

--- StaticPartition.h
+++ StaticPartition.h
@@ -1,6 +1,10 @@
 #ifndef STATIC_PARTITION_H
 #define STATIC_PARTITION_H
 
+#include "params.h"
+
+#include <cstdint>
+
 double getGPUTimeCandidates(int nbQueries, DTYPE epsilon, uint64_t nbCandidates);
 
 double getGPUTimeQueries(int nbQueries, DTYPE epsilon);
